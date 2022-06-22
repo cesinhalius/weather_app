a4367e579a1e225cb3f5c4235c294f6be83105f0
Cesar Augusto <cesinhalius@hotmail.com>
2022-06-22 19:49:17 -0300

    Adicionando estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function App() {
-  return (<h1>Oi Mundo</h1>);
+  return (
+    <div className="flex w-full h-screen items-center justify-center">
+      <form>
+        <input type="text" placeholder="Cidade" />
+        <button type="submit">Pesquisar</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
