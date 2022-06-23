4112ecc95e5a9134f8551a6c42d8e43c782c633f
Cesar Augusto <cesinhalius@hotmail.com>
2022-06-23 13:52:02 -0300

    Adicionando estilo ao input e botão

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,17 @@ function App() {
   return (
     <div className="flex w-full h-screen items-center justify-center">
       <form>
-        <input type="text" placeholder="Cidade" />
-        <button type="submit">Pesquisar</button>
+        <input
+          type="text"
+          placeholder="Cidade"
+          className="p-3 rounded-lg outline-none"
+        />
+        <button
+          type="submit"
+          className="bg-sky-600 p-3 rounded-lg ml-3 text-white font-bold"
+        >
+          Pesquisar
+        </button>
       </form>
     </div>
   );
