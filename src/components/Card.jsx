5d337af99200a244bd41c5c7562e428a7f5590a1
Cesar Augusto <cesinhalius@hotmail.com>
2022-06-23 15:37:07 -0300

    Adicionando card sobre o tempo

---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line linebreak-style
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable linebreak-style */
+import React from 'react';
+
+function Card() {
+  return (
+    <div className="bg-white p-6 mt-10 rounded-lg">
+      <div>
+        <span className="block text-xl font-bold text-slate-700">Cidade</span>
+        <span>Estado</span>
+        <span>País</span>
+      </div>
+
+      <div>
+        <span>27</span>
+        <span>C</span>
+      </div>
+
+      <div>
+        <span>icone</span>
+        <span>nublado</span>
+      </div>
+    </div>
+  );
+}
+
+// eslint-disable-next-line linebreak-style
+export default Card;
