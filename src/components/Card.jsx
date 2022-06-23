5db6f50cb4746447ba23e3524a0bb08de6af28ab
Cesar Augusto <cesinhalius@hotmail.com>
2022-06-23 16:22:19 -0300

    Adicionando estilo ao card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,21 +6,20 @@ import React from 'react';
 
 function Card() {
   return (
-    <div className="bg-white p-6 mt-10 rounded-lg">
-      <div>
+    <div className="bg-white p-6 mt-10 rounded-md border-1 border-slate-50 shadow-2xl shadow-black">
+      <div className="text-center">
         <span className="block text-xl font-bold text-slate-700">Cidade</span>
-        <span>Estado</span>
-        <span>País</span>
+        <span className="text-slate-400 text-sm font-medium">Estado</span>
       </div>
 
-      <div>
-        <span>27</span>
-        <span>C</span>
+      <div className="font-bold text-slate-700 flex mt-3 mb-2">
+        <span className="text-8xl">27</span>
+        <span className="text-3xl mt-2">ºC</span>
       </div>
 
-      <div>
-        <span>icone</span>
-        <span>nublado</span>
+      <div className="text-center">
+        <span className="block">icone</span>
+        <span className="text-slate-700 font-medium">nublado</span>
       </div>
     </div>
   );
