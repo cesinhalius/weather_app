c044950eaf32c5c9047ec8b7d3cbf157a4223359
Cesar Augusto <cesinhalius@hotmail.com>
2022-06-23 17:46:55 -0300

    Adicionando função do clima do tempo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+/* eslint-disable no-console */
 import React from 'react';
 import Card from './components/Card';
+import fetchData from './services/api';
 
 function App() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    fetchData('Campinas').then((resp) => {
+      console.log(resp);
+    });
+  };
+
   return (
     <div className="flex flex-col w-full h-screen items-center justify-center">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Cidade"
