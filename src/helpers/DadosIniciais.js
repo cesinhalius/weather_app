9d259c13468e18eb69f60b5f7562a3a76a088fa6
Cesar Augusto <cesinhalius@hotmail.com>
2022-06-24 18:32:44 -0300

    Adicionando os dados iniciais

---
+++ src/helpers/DadosIniciais.js
@@ -0,0 +1,17 @@
+/* eslint-disable linebreak-style */
+const dadoinicial = {
+  location: {
+    name: '-',
+    region: '-',
+    country: '-',
+  },
+  current: {
+    temp_c: 0,
+    condition: {
+      text: '-',
+      icon: '//cdn.weatherapi.com/weather/64x64/night/353.png',
+    },
+  },
+};
+
+export default dadoinicial;
