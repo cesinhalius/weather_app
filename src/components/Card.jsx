60f985e6217b60afe2a3d42818652134edab7a1a
Cesar Augusto <cesinhalius@hotmail.com>
2022-06-24 18:34:53 -0300

    Adicionando dados no card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,34 @@
 /* eslint-disable linebreak-style */
 import React from 'react';
+import propTypes from 'prop-types';
 
-function Card() {
+function Card({ data }) {
+  const {
+    location,
+    current,
+  } = data;
   return (
     <div className="bg-white p-6 mt-10 rounded-md border-1 border-slate-50 shadow-2xl shadow-black">
       <div className="text-center">
-        <span className="block text-xl font-bold text-slate-700">Cidade</span>
+        <span className="block text-xl font-bold text-slate-700">{location.name}</span>
+        <span className="text-slate-400 text-sm font-medium">{`${location.region}, ${location.country}`}</span>
       </div>
 
       <div className="font-bold text-slate-700 flex mt-3 mb-2">
-        <span className="text-8xl">27</span>
-        <span className="text-3xl mt-2">ºC</span>
+        <span className="text-8xl">{current.temp_c}</span>
+        <span className="text-3xl mt-2">°C</span>
       </div>
 
       <div className="text-center">
-        <span className="block">icone</span>
-        <span className="text-slate-700 font-medium">nublado</span>
+        <img src={current.condition.icon} alt="" />
+        <span className="text-slate-700 font-medium">{current.condition.text}</span>
       </div>
     </div>
   );
 }
 
 export default Card;
+
+Card.propTypes = {
+  data: propTypes.object,
+}.isRequired;
